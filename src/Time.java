14881d611d5caef2fde09c6bacf373d6a4c5e42e
SuzannaO <ostrovskaya.s3@gmail.com>
2021-09-13 16:32:54 -0700

    Time

--- src/Time.java
+++ src/Time.java
@@ -1,6 +1,16 @@
 public class Time {
     public static void main(String[] args) {
-
+        int time = 8;
+        if (time >= 8 && time <= 12)
+            System.out.println("Good Morning Sunshine!");
+        if (time >= 13 && time <= 19)
+            System.out.println("Good Afternoon. Work Hard!");
+        if (time >= 20 && time <= 24)
+            System.out.println("Good Evening. Get some rest!");
+        if (time < 0 || time > 24)
+            System.out.println("Invalid time meaning, check the settings.");
+        if (time >= 0 && time < 8)
+            System.out.println("Time to go to bed now!");
     }
 }
 
