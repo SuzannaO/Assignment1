a873877f8c29e68dd8b616b54d4334484497913e
SuzannaO <ostrovskaya.s3@gmail.com>
2021-09-13 13:20:52 -0700

    Initial commit

---
+++ src/Compare.java
@@ -0,0 +1,44 @@
+public class Compare {
+
+    public static void main(String[] args) {
+        short numberOne = 8;
+        short numberTwo = 5;
+        if (numberOne > numberTwo) System.out.println("Number One is larger, number Two is smaller");
+        else {
+            System.out.println("Number Two is larger, number One is smaller");
+        }
+        if (numberOne == numberTwo) {
+            System.out.println("Number one ");
+        } else {
+            System.out.println("Number One and number Two are equal");
+        }
+        short number1 = -5;
+        short number2 = 10;
+        if (number1 % 2 == 0) {
+            System.out.println("Number One is even");
+        } else
+            System.out.println("Number One is odd");
+        if (number2 % 2 == 0) {
+            System.out.println("Number Two is even");
+        } else
+            System.out.println("Number Two is odd");
+        if (number1 < 0) {
+            System.out.println("Number One is negative");
+        } else
+            System.out.println("Number One is positive");
+        if (number2 < 0) {
+            System.out.println("Number Two is negative");
+        } else
+            System.out.println("Number Two is positive");
+        if (number1 < 100) {
+            System.out.println("Number One is less than 100");
+        } else
+            System.out.println("Number One is more than 100");
+        if (number2 < 100) {
+            System.out.println("Number Two is more than 100");
+        }
+    }
+}
+
+
+
